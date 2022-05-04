c153bdceecc0d78e1bbdd89822c561cd4285d07c
annashv <annashivrina@gmail.com>
2022-05-04 18:07:35 +0200

    How to keep browser opened after test run in Selenide

--- src/test/java/tests/demoqa/TestBoxTests.java
+++ src/test/java/tests/demoqa/TestBoxTests.java
@@ -1,10 +1,20 @@
 package tests.demoqa;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestBoxTests {
+
+    @BeforeAll
+    static void setUp() {
+        //uncomment if you want to keep browser closed after test run
+        //Configuration.holdBrowserOpen = true;
+    }
+
     @Test
     void fillFormTest() {
         open("https://demoqa.com/text-box");
